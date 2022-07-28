3327addff5615fe5aec63da09fe5d9b78a5ec42c
Binary Beast <binary-beast@users.noreply.github.com>
2022-07-28 19:00:04 +0500

    added error in input forms

--- mimr7-angular/src/app/modules/accounts/login/login.component.ts
+++ mimr7-angular/src/app/modules/accounts/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { AuthenticationService } from 'src/app/shared/services/authentication.service';
 
@@ -9,41 +10,53 @@ import { AuthenticationService } from 'src/app/shared/services/authentication.se
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  form: FormGroup = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
+  loginFrom: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', Validators.required),
   });
+  returnUrl: string = '';
 
-  constructor(private auth: AuthenticationService,
-    private cookieService: CookieService) {}
+  constructor(
+    private auth: AuthenticationService,
+    private route: ActivatedRoute,
+    private cookieService: CookieService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
   submit() {
-    const exp = new Date(new Date().getTime() + 1000*60*60).toUTCString();
-    console.log('sss',exp)
-    this.auth
-      .login({ email: 'broek@crisisplan.nl', password: '12345678' })
-      .subscribe(
-        (res:any) => {
-          localStorage.setItem('token',res.token)
-          this.cookieService.set('Auth_token',res.token,{expires: 4})
-          this.auth.getUser().subscribe((res:any)=>{
-            console.log('get user',res)
-          },(err:any)=>{
-            console.log('error',err)
-          })
-        },
-        (err) => {
-          // console.log(err);
-        }
-      );
-    if (this.form.valid) {
-      // this.submitEM.emit(this.form.value);
+    console.log('login',this.loginFrom.valid,this.loginFrom.value)
+    if (this.loginFrom.valid) {
+      const exp = new Date(new Date().getTime() + 1000 * 60 * 60).toUTCString();
+      console.log('sss', exp);
+      this.auth
+        .login({ email: 'broek@crisisplan.nl', password: '12345678' })
+        .subscribe(
+          (res: any) => {
+            localStorage.setItem('token', res.token);
+            this.cookieService.set('Auth_token', res.token, { expires: 4 });
+            this.auth.getUser().subscribe(
+              (res: any) => {
+                localStorage.setItem('user', JSON.stringify(res));
+                this.router.navigateByUrl(this.returnUrl);
+              },
+              (err: any) => {
+                console.log('error', err);
+              }
+            );
+          },
+          (err) => {
+            console.log(err);
+          }
+        );
     }
   }
   // @Input() error: string | null | undefined;
 
   // @Output() submitEM = new EventEmitter();
-
-  ngOnInit(): void {}
 
   login() {
     // http://localhost:8081/auth/login
