ae93b66eec1efa1a6b1f83828e82213a3ebdf1c5
Binary Beast <binary-beast@users.noreply.github.com>
2022-08-18 18:26:14 +0500

    working on listing and state managemnt of organzaition

--- mimr7-angular/src/app/modules/organization/organization/organization.component.ts
+++ mimr7-angular/src/app/modules/organization/organization/organization.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { OrganizationModalComponent } from '../organization-modal/organization-modal.component';
+import { OrganizationService } from '../services/organization.service';
 
 @Component({
   selector: 'app-organization',
@@ -13,9 +14,14 @@ import { OrganizationModalComponent } from '../organization-modal/organization-m
 export class OrganizationComponent implements OnInit {
   path: string = 'Dashboard';
   active: string = 'Organizations';
-  constructor(public dialog: MatDialog) {}
+  constructor(
+    public dialog: MatDialog,
+    private organizationService: OrganizationService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getAllOrganizations();
+  }
 
   openOrganizationModal(): void {
     const dialogRef = this.dialog.open(OrganizationModalComponent, {
@@ -29,7 +35,12 @@ export class OrganizationComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe((result) => {
       console.log('The dialog was closed', result);
-      // this.animal = result;
+    });
+  }
+
+  getAllOrganizations() {
+    this.organizationService.getAll().subscribe((res) => {
+      console.log(res);
     });
   }
 }
--- mimr7-angular/src/app/modules/organization/services/organization.service.ts
+++ mimr7-angular/src/app/modules/organization/services/organization.service.ts
@@ -4,15 +4,28 @@ import { BehaviorSubject, Observable } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
-import { AddOrganization } from 'src/app/shared/interfaces';
+import { AddOrganization, Organization } from 'src/app/shared/interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OrganizationService {
+  private organizationState = new BehaviorSubject<Organization[]>([]);
+  organizationState$ = this.organizationState.asObservable();
   constructor(private http: HttpClient) {}
+
+  getUserState() {
+    return this.organizationState$;
+  }
+  setUserState(organization: Organization[]) {
+    this.organizationState.next(organization);
+  }
 
   addOrganization(data: AddOrganization): Observable<any> {
     return this.http.post(`${environment.apiUrl}accounts`, data);
+  }
+
+  getAll() {
+    return this.http.get(`${environment.apiUrl}accounts`);
   }
 }
