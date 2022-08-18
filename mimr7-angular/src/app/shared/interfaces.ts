48aa51afe41110ebc651287023114bbe254a919a
Binary Beast <binary-beast@users.noreply.github.com>
2022-08-18 18:24:14 +0500

    interface for organization

--- mimr7-angular/src/app/shared/interfaces.ts
+++ mimr7-angular/src/app/shared/interfaces.ts
@@ -14,3 +14,21 @@ export interface AddOrganization {
     mobilePhone?: String;
   };
 }
+export interface Organization {
+  id: String;
+  name: String;
+  description: String;
+  apikey: String;
+  organizationName?: String;
+  avatar?: String;
+  type?: String;
+  category_header?: String;
+  messages_font_size?: String;
+  messages_font_family?: String;
+  status: boolean;
+  public: boolean;
+  active: boolean;
+  createdAt: String;
+  updatedAt: String;
+  deletedAt?: String;
+}
