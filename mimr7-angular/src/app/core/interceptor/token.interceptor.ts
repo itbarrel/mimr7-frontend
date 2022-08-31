ffd9eb17c2f8d29c1f2b3e35deb18d4f61ff3954
Binary Beast <binary-beast@users.noreply.github.com>
2022-08-31 15:59:58 +0500

    Refactored and fix cors issue

--- mimr7-angular/src/app/core/interceptor/token.interceptor.ts
+++ mimr7-angular/src/app/core/interceptor/token.interceptor.ts
@@ -4,13 +4,9 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
-  HttpResponse,
-  HttpErrorResponse,
   HttpHeaders,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
@@ -21,15 +17,6 @@ export class TokenInterceptor implements HttpInterceptor {
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    // let token = '';
-    // if (localStorage.getItem('token')) {
-    //   token = JSON.parse(
-    //     this.encrypt.decrypt(localStorage.getItem('user') || '')
-    //   ).api_key;
-    //   console.log(
-    //     JSON.parse(this.encrypt.decrypt(localStorage.getItem('user') || ''))
-    //   );
-    // }
 
     //set headers
     const customReq = request.clone({
@@ -37,26 +24,8 @@ export class TokenInterceptor implements HttpInterceptor {
       headers: new HttpHeaders({
         token: localStorage.getItem('token') || '',
         'Content-Type':'application/json'
-        // token: environment.companyToken,
       }),
     });
     return next.handle(customReq)
-
-    // pass on the modified request object
-    // return next.handle(customReq).pipe(
-    //   tap(
-    //     (event) => {
-    //       if (event instanceof HttpResponse) {
-    //         this.loader.hide();
-    //       }
-    //     },
-    //     (error) => {
-    //       if (error instanceof HttpErrorResponse) {
-    //         this.loader.hide();
-    //         this.toaster.showError(error.error.error);
-    //       }
-    //     }
-    //   )
-    // );
   }
 }
