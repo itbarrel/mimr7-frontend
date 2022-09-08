f97d6d22e93469c0feb7bcf63ee7b4422c0aaf06
Binary Beast <binary-beast@users.noreply.github.com>
2022-09-08 23:35:21 +0500

    admin role permission set

--- mimr7-angular/src/assets/data/permission.ts
+++ mimr7-angular/src/assets/data/permission.ts
@@ -3,6 +3,14 @@ export const PERMISSION = [
     role: 'SuperAdmin',
     sites: ['accounts'],
     default: 'accounts',
-    navigation: [{ name: 'Accounts', url: 'accounts',icon:'corporate_fare' }]
+    navigation: [{ name: 'Accounts', url: 'accounts', icon: 'corporate_fare' }],
+  },
+  {
+    role: 'Admin',
+    sites: ['organizations'],
+    default: 'organizations',
+    navigation: [
+      { name: 'Organizations', url: 'organizations', icon: 'corporate_fare' },
+    ],
   },
 ];
