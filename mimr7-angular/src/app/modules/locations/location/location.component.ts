c537dbdedc59b7171cf7d27357a7bd5ed627f853
Binary Beast <binary-beast@users.noreply.github.com>
2022-11-01 16:27:28 +0500

    active/inactive tab on home screen

--- mimr7-angular/src/app/modules/locations/location/location.component.ts
+++ mimr7-angular/src/app/modules/locations/location/location.component.ts
@@ -64,7 +64,7 @@ export class LocationComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.breadCrumbService.setrouteState('Collections');
+    this.breadCrumbService.setrouteState('Locations');
 
     this.getAllLocations();
   }
