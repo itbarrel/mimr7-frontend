1f338092dec48d0b6bd08cd330d04bff9d2b0c3e
Binary Beast <binary-beast@users.noreply.github.com>
2023-01-03 17:56:52 +0500

    moving organization in student module listing done

---
+++ mimr7-angular/src/app/modules/student/organization/organization.component.ts
@@ -0,0 +1,130 @@
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { OrganizationService } from '../services/organization.service';
+import { MatDialog } from '@angular/material/dialog';
+import {
+  Observable,
+  of as observableOf,
+  BehaviorSubject,
+  combineLatest,
+} from 'rxjs';
+import { catchError, map, startWith, switchMap } from 'rxjs/operators';
+import { Organization } from 'src/app/shared/interfaces';
+import { BreadCrumbService } from 'src/app/shared/services/breadcrumb.service';
+
+@Component({
+  selector: 'app-organization',
+  templateUrl: './organization.component.html',
+  styleUrls: ['./organization.component.scss']
+})
+export class OrganizationComponent implements OnInit {
+
+  path: string = 'Dashboard';
+  active: string = 'Organizations';
+  isLoadingResults = true;
+  resultsLength = 0;
+  name = '';
+
+  displayedColumns: string[] = [
+    'name',
+    'status',
+    'city',
+    'createdAt',
+    'action',
+  ];
+  pageSize = 10;
+  data!: Observable<any[]>;
+  currentPage = new BehaviorSubject<number>(1);
+  page = new BehaviorSubject<any>({
+    pageSize: this.pageSize,
+    pageIndex: 0,
+  });
+  currentSort = new BehaviorSubject<MatSort>({} as MatSort);
+  pageSizeOptions: number[] = [5, 10, 25, 50, 100];
+
+  @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort: MatSort = {} as MatSort;
+
+  constructor(
+    private organizationService: OrganizationService,
+    private dialog: MatDialog,
+    private breadCrumbService: BreadCrumbService
+  ) {}
+
+  ngOnInit(): void {
+    this.breadCrumbService.setrouteState('Organizations');
+    this.getAllOrganizations();
+  }
+
+  getAllOrganizations() {
+    this.organizationService
+      .getAll(1, 10, this.currentSort)
+      .subscribe((res) => {
+        console.log(res);
+      });
+    this.data = combineLatest(this.currentSort, this.page).pipe(
+      // startWith([undefined, ]),
+      switchMap(([sortChange, page]) => {
+        console.log('PAge Change', sortChange, page);
+        this.isLoadingResults = true;
+        return this.organizationService.getAll(
+          page.pageIndex + 1,
+          page.pageSize,
+          sortChange,
+          this.name
+        );
+        // return this.exampleDatabase.getRepoIssues(
+        //   this.sort.active, this.sort.direction, currentPage);
+      }),
+      map((data: any) => {
+        // Flip flag to show that loading has finished.
+        this.isLoadingResults = false;
+        this.resultsLength = data.total;
+
+        return data.data;
+      }),
+      catchError(() => {
+        this.isLoadingResults = false;
+        // Catch if the GitHub API has reached its rate limit. Return empty data.
+        return observableOf([]);
+      })
+    );
+  }
+
+  applySort(sort: any) {
+    this.currentSort.next(sort);
+  }
+  pageChanged(pageChanged: any) {
+    this.page.next(pageChanged);
+    console.log('called', pageChanged);
+  }
+
+  searchByFilters() {
+    console.log('filter', this.name);
+    this.getAllOrganizations();
+  }
+  clearFilters() {
+    this.name = '';
+    this.getAllOrganizations();
+  }
+
+  // openOrganizationModal(data?: Organization): void {
+  //   const dialogRef = this.dialog.open(OrganizationModalComponent, {
+  //     width: '50%',
+  //     // minHeight: 'calc(100vh - 90px)',
+  //     data,
+  //     autoFocus: false,
+  //     height: 'auto',
+  //     panelClass: 'custom-dialog-container',
+  //     disableClose: true,
+  //   });
+
+  //   dialogRef.afterClosed().subscribe((result: any) => {
+  //     console.log('The dialog was closed', result);
+  //     if (result.success) {
+  //       this.getAllOrganizations();
+  //     }
+  //   });
+  // }
+}
---
+++ mimr7-angular/src/app/modules/student/services/organization.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+import { Organization } from 'src/app/shared/interfaces';
+// import { WithQueryOptions } from 'with-query/dist';
+import { withQuery } from 'with-query';
+import { AuthenticationService } from 'src/app/shared/services/authentication.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OrganizationService {
+  private organizationState = new BehaviorSubject<Organization[]>([]);
+  organizationState$ = this.organizationState.asObservable();
+  user: any;
+  constructor(private http: HttpClient,private auth:AuthenticationService) {
+    this.auth.getUserState().subscribe((res) => {
+      this.user = res
+    });
+  }
+
+  getOrganizationState() {
+    return this.organizationState$;
+  }
+  setOrganizationState(organization: Organization[]) {
+    this.organizationState.next(organization);
+  }
+
+  addOrganization(data: Organization): Observable<any> {
+    data.AccountId=this.user.AccountId
+    return this.http.post(`${environment.apiUrl}organizations`, data);
+  }
+
+  getAll(
+    pageNumber: number,
+    pageSize: number,
+    sortChange?: any,
+    name?: string
+  ) {
+    const sort: any = {};
+    if(sortChange){
+      sort[sortChange.active] = sortChange.direction;
+    }
+    const query = {
+      sort,
+    };
+
+    const url = withQuery(
+      `${environment.apiUrl}organizations?offset=${pageNumber}&limit=${pageSize}&name=${name}`,
+      query
+    );
+    return this.http.get(url);
+  }
+  getOrganizationById(id: string) {
+    return this.http.get(`${environment.apiUrl}organizations/${id}`);
+  }
+  updateOrganization(id: string, data: Organization) {
+    return this.http.put(`${environment.apiUrl}organizations/${id}`, data);
+  }
+}
