843648153d0dfcf5d8d68b083b5fca49fe2b72b4
Binary Beast <binary-beast@users.noreply.github.com>
2023-01-18 13:21:11 +0500

    added aditional parameter to filter content and student respectively

--- mimr7-angular/src/app/shared/pipes/FilterPipe.pipe.ts
+++ mimr7-angular/src/app/shared/pipes/FilterPipe.pipe.ts
@@ -5,16 +5,23 @@ import { Pipe, PipeTransform } from '@angular/core';
   pure: false,
 })
 export class FilterPipe implements PipeTransform {
-  transform(items: any[], filter: string): any {
+  transform(items: any[], filter: string, key: string): any {
     if (!items || !filter) {
       return items;
     }
+    if (key === 'students') {
+      return items.filter((item) =>
+        item.name.toLowerCase().includes(filter.toLowerCase())
+      );
+    } else {
+      console.log(items);
+      return items.filter((item) =>
+        item.title.toLowerCase().includes(filter.toLowerCase())
+      );
+    }
 
-    console.log(items, '---pipe---', filter);
+    // console.log(items, '---pipe---', filter);
     // filter items array, items which match and return true will be
     // kept, false will be filtered out
-    return items.filter((item) =>
-      item.name.toLowerCase().includes(filter.toLowerCase())
-    );
   }
 }
