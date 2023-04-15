011ef99c669b3b6ea1033bda61da1670cb6b39a2
Binary Beast <binary-beast@users.noreply.github.com>
2023-04-15 13:37:55 +0500

    added generic confirmation dialogue

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { FileUploadComponent } from "./file-upload/file-upload.component";
 import { BreadcrumbComponent } from "./breadcrumb/breadcrumb.component";
 import { SharedModule } from "../shared.module";
+import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
 
 @NgModule({
-  declarations: [FileUploadComponent, BreadcrumbComponent],
+  declarations: [FileUploadComponent, BreadcrumbComponent, ConfirmationDialogComponent],
   imports: [SharedModule],
-  exports: [FileUploadComponent, BreadcrumbComponent],
+  exports: [FileUploadComponent, BreadcrumbComponent,ConfirmationDialogComponent],
 })
 export class ComponentsModule {}
